1c4a52cc999e6ac3d2b1b2f115376cce058faf6b
Lukiiy <lukiiy@proton.me>
2026-09-08 12:39:16 -0300

    oh look, more vars & config handling

--- src/main/kotlin/me/lukiiy/nameplates/Nameplates.kt
+++ src/main/kotlin/me/lukiiy/nameplates/Nameplates.kt
@@ -5,14 +5,27 @@ import org.bukkit.plugin.java.JavaPlugin
 class Nameplates : JavaPlugin() {
     lateinit var manager: NametagManager
 
-    val viewDist: Int = 32
+    var viewDist: Int = 0
+    var updateTicks: Long = 20
+    var verticalOffset: Double = 0.0
 
     override fun onEnable() {
+        saveDefaultConfig()
+        getConfig().options().copyDefaults(true)
+        saveConfig()
+        reloadComms()
+
         manager = NametagManager()
     }
 
     companion object {
         val instance: Nameplates
             get() = getPlugin(Nameplates::class.java)
+    }
+
+    internal fun reloadComms() {
+        viewDist = config.getInt("viewDist", 32)
+        updateTicks = config.getLong("updateTicks", 20)
+        verticalOffset = config.getDouble("verticalOffset", .35)
     }
 }
