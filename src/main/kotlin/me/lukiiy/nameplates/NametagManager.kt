271f2ca44c220efe35d2a3247ff8a32b5252eb70
Lukiiy <lukiiy@proton.me>
2026-09-11 09:23:39 -0300

    oh

--- src/main/kotlin/me/lukiiy/nameplates/NametagManager.kt
+++ src/main/kotlin/me/lukiiy/nameplates/NametagManager.kt
@@ -2,6 +2,8 @@ package me.lukiiy.nameplates
 
 import me.lukiiy.nameplates.Utils.asNMS
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.minecraft.network.protocol.Packet
 import org.bukkit.entity.Player
@@ -83,7 +85,6 @@ class NametagManager {
             }
         }
 
-        val lastIdx = group.size - 1
         for (i in group.indices) {
             group[i].setText(parts[i])
             group[i].sneak(player.isSneaking)
