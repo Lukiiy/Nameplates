db5e214db7c58dc5e9e9175cfbd318896e94ea1a
Lukiiy <lukiiy@proton.me>
2026-09-11 00:38:21 -0300

    Use.... Interactione entities?!?

--- src/main/kotlin/me/lukiiy/nameplates/PlateEntity.kt
+++ src/main/kotlin/me/lukiiy/nameplates/PlateEntity.kt
@@ -1,33 +1,28 @@
 package me.lukiiy.nameplates
 
 import com.google.gson.JsonParser
-import com.mojang.math.Transformation
 import com.mojang.serialization.JsonOps
 import me.lukiiy.nameplates.Utils.asNMS
 import net.kyori.adventure.text.Component as AdventureComponent
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import net.minecraft.network.chat.ComponentSerialization
-import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket
 import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
-import net.minecraft.world.entity.Display
 import net.minecraft.world.entity.EntityTypes
+import net.minecraft.world.entity.Interaction
 import org.bukkit.entity.Player
-import org.joml.Quaternionf
-import org.joml.Vector3f
 
-class PlateEntity(owner: Player) {
-    private val handle: Display.TextDisplay = Display.TextDisplay(EntityTypes.TEXT_DISPLAY, owner.asNMS().level())
+class PlateEntity(val owner: Player, lineIndex: Int, totalLines: Int) {
+    private val handle = Interaction(EntityTypes.INTERACTION, owner.asNMS().level())
 
     init {
         handle.apply {
-            setPos(owner.x, owner.y, owner.z)
+            width = 0.0f
+            height = ((totalLines - 1 - lineIndex) * Nameplates.instance.lineGap).toFloat().coerceAtLeast(0.0f)
 
-            billboardConstraints = Display.BillboardConstraints.CENTER
-            shadowStrength = 0f
-            viewRange = 1f
-            transformationInterpolationDuration = 0
+            isCustomNameVisible = true
+            isInvulnerable = true
         }
     }
 
@@ -37,16 +32,14 @@ class PlateEntity(owner: Player) {
     fun setText(text: AdventureComponent) {
         val json = JsonParser.parseString(GsonComponentSerializer.gson().serialize(text))
 
-        handle.text = ComponentSerialization.CODEC.parse(JsonOps.INSTANCE, json).getOrThrow { IllegalStateException(it) }
+        handle.customName = ComponentSerialization.CODEC.parse(JsonOps.INSTANCE, json).getOrThrow { IllegalStateException(it) }
     }
 
-    fun setOffset(y: Float) = handle.setTransformation(Transformation(Vector3f(0f, y, 0f), Quaternionf(), Vector3f(1f, 1f, 1f), Quaternionf()))
-
-    fun setSneak(sneaking: Boolean) {
-        handle.textOpacity = if (sneaking) 0x59 else -1 // seems accurate
+    fun sneak(isSneaking: Boolean) {
+        handle.isShiftKeyDown = isSneaking
     }
 
-    fun spawnPacket(): Packet<*> = ClientboundAddEntityPacket(handle, 0, handle.blockPosition())
-    fun metadataPacket(): Packet<*> = ClientboundSetEntityDataPacket(handle.id, handle.entityData.packDirty() ?: handle.entityData.packAll())
-    fun removePacket(): Packet<*> = ClientboundRemoveEntitiesPacket(handle.id)
+    fun spawnPacket() = ClientboundAddEntityPacket(handle, 0, owner.asNMS().blockPosition())
+    fun metadataPacket() = ClientboundSetEntityDataPacket(handle.id, handle.entityData.packDirty() ?: handle.entityData.packAll())
+    fun removePacket() = ClientboundRemoveEntitiesPacket(handle.id)
 }
